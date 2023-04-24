b1ba6abdf5f0991239202a60f0c6d6b1d460bce8
AndresRomero2001 <androm10@ucm.es>
2023-04-24 12:06:47 +0200

    mejoras perfil

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -69,7 +69,7 @@ const modifyProfileModal = new bootstrap.Modal(document.querySelector('#modifyPr
     }
 } */
 
-function applyChanges(){
+/* function applyChanges(){
     console.log("-- dentro de apply changes --");
     const id = document.getElementById("userId").value;
     const oldUsername = document.getElementById("oldUsername").value;
@@ -114,6 +114,61 @@ function applyChanges(){
             document.getElementById("password1").value="";
             document.getElementById("password2").value="";
             document.getElementById('closeProfileModal').click(); // cutre, pero lo de arriba no me ha funcionado
+        })
+        .catch(() => {console.log("Error en catch apply changes");//si el username ya existia
+            username.setCustomValidity("El usuario ya existe, escoja otro, por favor");
+            username.reportValidity();
+        })
+    }
+
+    
+} */
+
+function applyChanges(){
+    console.log("-- dentro de apply changes --");
+    const id = document.getElementById("userId").value;
+    const oldUsername = document.getElementById("oldUsername").value;
+    const myForm = document.getElementById("modifyProfileForm");
+    let username = document.getElementById("username");
+
+    username.setCustomValidity("");
+    console.log("el id guardado es: " +  id);
+
+    if(!myForm.checkValidity())//comprueba si se cumplen las condiciones html (required, longitud maxima, formato, etc)
+    {
+        //si alguna condicion no se cumplia, llamamos a la funcion que muestra automaticamente un mensaje donde estuviera el primer error
+        myForm.reportValidity();
+    }
+    else{
+        let username = document.getElementById("username");
+        let oldUsername = document.getElementById("oldUsername");
+        let firstName = document.getElementById("firstName");
+        let lastName = document.getElementById("lastName");
+        let password1 = document.getElementById("password1");
+        let password2 = document.getElementById("password2");
+        let address = document.getElementById("address");
+        let phone = document.getElementById("phone");
+        let email = document.getElementById("email");
+
+        let params = {"username" : username.value,
+                    "oldUsername" : oldUsername.value,
+                    "firstName" : firstName.value,
+                    "lastName" : lastName.value,
+                    "password1" : password1.value,
+                    "password2" : password2.value,
+                    "address" : address.value,
+                    "phone" : phone.value,
+                    "email" : email.value,
+                    "id" : id
+        }; 
+
+        go(config.rootUrl + "/modifyUser", 'POST', params)
+        .then(d => {console.log("todo ok") // va ok si el username no existe o si existe pero era el del user correspondiente
+            document.getElementById("oldUsername").value = d["username"];
+            username.setCustomValidity("");
+            document.getElementById("password1").value="";
+            document.getElementById("password2").value="";
+            //document.getElementById('closeProfileModal').click(); // cutre, pero lo de arriba no me ha funcionado
         })
         .catch(() => {console.log("Error en catch apply changes");//si el username ya existia
             username.setCustomValidity("El usuario ya existe, escoja otro, por favor");
